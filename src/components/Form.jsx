33ce8bdf1b701650ad7fdb8d152727a36aaa1d75
Zinc4 <65228679+Zinc4@users.noreply.github.com>
2023-12-05 17:05:47 +0700

    Upload project to github

---
+++ src/components/Form.jsx
@@ -0,0 +1,139 @@
+// "https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=0&longitude=0"
+
+import { useEffect, useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import styles from "./Form.module.css";
+import Button from "./Button";
+import { useNavigate } from "react-router-dom";
+import BackButton from "./BackButton";
+import { useUrlPosition } from "../hooks/useUrlPosition";
+import Spinner from "./Spinner";
+import Message from "./Message";
+import { useCities } from "../contexts/CitiesContexts";
+
+export function convertToEmoji(countryCode) {
+  const codePoints = countryCode
+    .toUpperCase()
+    .split("")
+    .map((char) => 127397 + char.charCodeAt());
+  return String.fromCodePoint(...codePoints);
+}
+
+const BASE_URL = "https://api.bigdatacloud.net/data/reverse-geocode-client?";
+
+function Form() {
+  const [cityName, setCityName] = useState("");
+  const [country, setCountry] = useState("");
+  const [date, setDate] = useState(new Date());
+  const [notes, setNotes] = useState("");
+  const [isLoadingGeoLocation, setIsLoadingGeoLocation] = useState(false);
+  const [ErrorGeoLocation, setErrorGeoLocation] = useState("");
+  const [emoji, setEmoji] = useState("");
+
+  const [lat, lng] = useUrlPosition();
+
+  const { createCity, isLoading } = useCities();
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!lat && !lng) return;
+
+    async function fetchPosition() {
+      try {
+        setIsLoadingGeoLocation(true);
+        setErrorGeoLocation("");
+        const res = await fetch(`${BASE_URL}latitude=${lat}&longitude=${lng}`);
+
+        const data = await res.json();
+
+        setCityName(data.city);
+        setCountry(data.countryName);
+
+        setEmoji(convertToEmoji(data.countryCode));
+        if (!data.city)
+          throw new Error(
+            "That doesn't seem to be a city. Click somewhere else😉"
+          );
+      } catch (error) {
+        setErrorGeoLocation(error.message);
+      } finally {
+        setIsLoadingGeoLocation(false);
+      }
+    }
+
+    fetchPosition();
+  }, [lat, lng]);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!cityName || !date) return;
+
+    const newCity = {
+      cityName,
+      country,
+      emoji,
+      date,
+      notes,
+      position: { lat, lng },
+    };
+
+    await createCity(newCity);
+    navigate("/app/cities");
+  }
+
+  if (isLoadingGeoLocation) return <Spinner />;
+  if (ErrorGeoLocation) return <Message message={ErrorGeoLocation} />;
+  if (!lat && !lng)
+    return <Message message="Start by clicking somewhere on the map" />;
+
+  return (
+    <form
+      className={`${styles.form} ${isLoading ? styles.loading : ""}`}
+      onSubmit={handleSubmit}
+    >
+      <div className={styles.row}>
+        <label htmlFor="cityName">City name</label>
+        <input
+          id="cityName"
+          onChange={(e) => setCityName(e.target.value)}
+          value={cityName}
+        />
+        <span className={styles.flag}>{emoji}</span>
+      </div>
+
+      <div className={styles.row}>
+        <label htmlFor="date">When did you go to {cityName}?</label>
+        {/* <input
+          id="date"
+          onChange={(e) => setDate(e.target.value)}
+          value={date}
+        /> */}
+        <DatePicker
+          onChange={(date) => setDate(date)}
+          selected={date}
+          dateFormat="dd/MM/yyyy"
+          id="date"
+        />
+      </div>
+
+      <div className={styles.row}>
+        <label htmlFor="notes">Notes about your trip to {cityName}</label>
+        <textarea
+          id="notes"
+          onChange={(e) => setNotes(e.target.value)}
+          value={notes}
+        />
+      </div>
+
+      <div className={styles.buttons}>
+        <Button type="primary">Add</Button>
+        <BackButton />
+      </div>
+    </form>
+  );
+}
+
+export default Form;
